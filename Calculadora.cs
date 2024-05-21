4dc3df7578d6ac465216e72005b4d65ea770c0aa
Mikael <lescanostisman.mikael@gmail.com>
2024-05-21 13:28:29 -0300

    ejercicio 1

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,33 +1,29 @@
 using System.Security.Cryptography.X509Certificates;
 
-namespace espacioCalculadora
+namespace calculadoraNamespace;
+
+public class Calculadora
 {
-    public class Calculadora
-    {
-        private double resultado = 0;
-        public double RESULTADO
-        {
-            get;
-        }
-        public void Sumar(double termino)
-        {
-            resultado += termino;
-        }
-        public void Restar(double termino)
-        {
-            resultado -= termino;
-        }
-        public void Multiplicar(double termino)
-        {
-            resultado *= termino;
-        }
-        public void Dividir(double termino)
-        {
-            resultado /= 0;
-        }
-        public void Limpiar()
-        {
-            resultado = 0;
-        }
+    private double resultado;
+    public double Resultado {
+        get => resultado;
+    }
+    public void Sumar(double termino) {
+        resultado += termino;
+    }
+    public void Restar(double termino) {
+        resultado -= termino;
+    }
+    public void Multiplicar(double termino) {
+        resultado *= termino;
+    }
+    public void Dividir(double termino) {
+        resultado /= termino;
+    }
+    public void Limpiar() {
+        resultado = 0;
+    }
+    public Calculadora() {
+        resultado = 0;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,51 @@
 ﻿using System.Xml.Serialization;
-using espacioCalculadora;
+using calculadoraNamespace;
+using Microsoft.VisualBasic.FileIO;
 
-Console.WriteLine("### Calculadora");
+Console.WriteLine("Calculadora");
+Console.WriteLine("\n  Operaciones:");
+Console.WriteLine("    + (Suma)\n    - (Resta)\n    * (Multiplicacion)\n    / (Division)");
+Console.WriteLine("    L (Limpiar)\n    N (Salir de Calculadora)");
+
+string op;
+double num;
+
+Calculadora C = new Calculadora();
+
+do
+{
+    Console.WriteLine("Ingrese una operacion (+, - , *, /): ");
+    op = Console.ReadLine();
+    if (op == "N")
+        break;
+    Console.WriteLine("Ingrese un numero: ");
+    num = double.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case "+":
+            C.Sumar(num);
+            break;
+        case "-":
+            C.Restar(num);
+            break;
+        case "*":
+            C.Multiplicar(num);
+            break;
+        case "/":
+            if (num != 0)
+                C.Dividir(num);
+            else
+                Console.WriteLine("No se puede dividir por 0!");
+            break;
+        case "L":
+            C.Limpiar();
+            break;
+        case "N":
+            break;
+        default:
+            Console.WriteLine("Operador Invalido!");
+            break;
+    }
+} while (op != "N");
+
+Console.WriteLine("\nRESULTADO: "+C.Resultado);
